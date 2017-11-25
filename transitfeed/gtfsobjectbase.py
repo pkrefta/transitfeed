6fb324852c85b04f966f20136580cf8c8ca71d98
Gaston Avila <avila.gas@gmail.com>
2017-11-25 18:26:44 -0300

    stringio and string_types fixes

--- transitfeed/gtfsobjectbase.py
+++ transitfeed/gtfsobjectbase.py
@@ -69,7 +69,7 @@ class GtfsObjectBase(GtfsFactoryUser):
 
   def iteritems(self):
     """Return a iterable for (name, value) pairs of public attributes."""
-    for name, value in self.__dict__.iteritems():
+    for name, value in self.__dict__.items():
       if (not name) or name[0] == "_":
         continue
       yield name, value
